5835532eee35f21f6f0ea45d727ad84a15a5c1db
Denis Vitez <denis.vitez@gmail.com>
2020-01-18 00:18:33 +0100

    Added multiprocessing.

--- genetic.py
+++ genetic.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot
 from scipy.integrate import odeint
 import libs
 import time
+import multiprocessing as mp
+import sys
 
 
 #The GA methods are taken from https://github.com/ahmedfgad/GeneticAlgorithmPython
@@ -47,14 +49,13 @@ def mutation(offspring_crossover, num_mutations=1):
     return offspring_crossover
 
 
-def calculate_fitness_for_model(results):
+def calculate_fitness_for_specimen(results):
     #Currently we count the number of combinations that oscilate
     flat = np.ravel(results)
     osc_count = np.count_nonzero(flat == 1)
-    return  osc_count
+    return osc_count
 
-
-def calculate_results_for_model(model, nA, nB, nC):
+def calculate_results_for_specimen(model, nA, nB, nC):
     new = []
     new_amp = []
     new_per = []
@@ -73,13 +74,41 @@ def calculate_results_for_model(model, nA, nB, nC):
 def calculate_results_for_generation(model, population):
     pop_res = []
     for pop in population:
-        specimen_result = calculate_results_for_model(model, pop[0], pop[1], pop[2])
-        specimen_fitness = calculate_fitness_for_model(specimen_result)
+        specimen_result = calculate_results_for_specimen(model, pop[0], pop[1], pop[2])
+        specimen_fitness = calculate_fitness_for_specimen(specimen_result)
         print("Fitness calculated:", specimen_fitness)
         pop_res.append(specimen_fitness)
     return pop_res
 
-#Params
+
+def calculate_results_for_specimen_mp(params):
+    model, nA, nB, nC = params
+    new = []
+    new_amp = []
+    new_per = []
+    logspace_vector = np.logspace(0, 5, 1000).astype(int)
+    for j in range(0, len(logspace_vector)):
+        params = (alpha, alpha0, beta, n, nA, nB, nC, logspace_vector[j], logspace_vector[j], logspace_vector[j])
+        Z = odeint(model, Z0, t, args=params)
+        A = Z[:, 3]
+        is_osc, period, amplitude = libs.osc_detect(A)
+        new.append(is_osc)
+        new_amp.append(amplitude)
+        new_per.append(period)
+    fitness = calculate_fitness_for_specimen(new)
+    return fitness
+
+
+def calculate_results_for_generation_mp(model, population):
+    pop_res = []
+    work = []
+    for pop in population:
+        work.append((model, pop[0], pop[1], pop[2]))
+    p = mp.Pool(num_processes)
+    mp_solutions = p.map(calculate_results_for_specimen_mp, work)
+    return mp_solutions
+
+# Params
 Z0 = [ 7.02028482, 1.57288111, 58.50876295, 8.69333201, 1.40155783, 53.49915358]
 # number of time points
 t_end = 100
@@ -95,39 +124,44 @@ beta = 5
 num_weights = 3
 num_parents_mating = 4
 
-# Defining the population size.
-pop_size = (10, num_weights)
-# Creating the initial population.
-new_population = np.random.randint(low=1, high=5, size=pop_size)
-print(new_population)
-best_outputs = []
-num_generations = 1
+if __name__ == "__main__":
+    # Defining the population size.
+    pop_size = (20, num_weights)
+    # Creating the initial population.
+    new_population = np.random.randint(low=1, high=5, size=pop_size)
+    print(new_population)
+    best_outputs = []
+    num_generations = 10
 
-for generation in range(num_generations):
-    print("#### STARTED generation", generation, "####")
-    start = time.time()
-    population_fitness = calculate_results_for_generation(libs.extended_model_1, new_population)
-    print("Population fitness:")
-    print(population_fitness)
-    end = time.time()
-    print("Generation execution time:", end - start)
-    best_specimen = np.max(population_fitness)
-    best_outputs.append(best_specimen)
-    parents = select_mating_pool(new_population, population_fitness, num_parents_mating)
-    print("Parents")
-    print(parents)
-    offspring_crossover = crossover(parents, offspring_size=(pop_size[0] - parents.shape[0], num_weights))
-    print("Crossover")
-    print(offspring_crossover)
-    # Adding some variations to the offspring using mutation.
-    offspring_mutation = mutation(offspring_crossover, num_mutations=2)
-    print("Mutation")
-    print(offspring_mutation)
-    # Creating the new population based on the parents and offspring.
-    new_population[0:parents.shape[0], :] = parents
-    new_population[parents.shape[0]:, :] = offspring_mutation
-    print("New population:", new_population)
-    print("#### FINISHED generation", generation, "####")
+    # Parameters for multi processing
+    num_processes = 8
 
-print("Best values:", best_outputs)
+
+    for generation in range(num_generations):
+        print("#### STARTED generation", generation, "####")
+        start = time.time()
+        population_fitness = calculate_results_for_generation_mp(libs.extended_model_1, new_population)
+        print("Population fitness:")
+        print(population_fitness)
+        end = time.time()
+        print("Generation execution time:", end - start)
+        best_specimen = np.max(population_fitness)
+        best_outputs.append(best_specimen)
+        parents = select_mating_pool(new_population, population_fitness, num_parents_mating)
+        print("Parents")
+        print(parents)
+        offspring_crossover = crossover(parents, offspring_size=(pop_size[0] - parents.shape[0], num_weights))
+        print("Crossover")
+        print(offspring_crossover)
+        # Adding some variations to the offspring using mutation.
+        offspring_mutation = mutation(offspring_crossover, num_mutations=2)
+        print("Mutation")
+        print(offspring_mutation)
+        # Creating the new population based on the parents and offspring.
+        new_population[0:parents.shape[0], :] = parents
+        new_population[parents.shape[0]:, :] = offspring_mutation
+        print("New population:", new_population)
+        print("#### FINISHED generation", generation, "####")
+
+    print("Best values:", best_outputs)
 
