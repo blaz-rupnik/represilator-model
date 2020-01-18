aedcb79c3ff4a32031cb6d3c31ab1d24169b0253
Blaž <blaz.rupnik56@gmail.com>
2020-01-18 19:15:12 +0100

    Fixed color map and type for heatmaps

--- heatmapGenerator.py
+++ heatmapGenerator.py
@@ -16,7 +16,8 @@ def drawOscilationGraph(file, name, show):
 def drawAmplitudeGraph(file, name, show):
     data = np.loadtxt(file, delimiter=',')
     print(data)
-    plt.imshow(data, cmap="viridis", interpolation='none', aspect='auto')
+    plt.imshow(data, cmap="YlGnBu", interpolation='none', aspect='auto')
+    plt.colorbar()
     plt.yticks([])
     if show:
         plt.show()
@@ -26,14 +27,15 @@ def drawAmplitudeGraph(file, name, show):
 def drawPeriodGraph(file, name, show):
     data = np.loadtxt(file, delimiter=',')
     print(data)
-    plt.imshow(data, cmap="viridis", interpolation='none', aspect='auto')
+    plt.imshow(data, cmap="YlGnBu", interpolation='none', aspect='auto')
+    plt.colorbar()
     plt.yticks([])
     if show:
         plt.show()
     timestr = time.strftime("%Y%m%d-%H%M%S")
     plt.savefig('img/'+name+"_"+timestr+".png")
 
-drawOscilationGraph('extended_model_1_v3.txt', "model_1_oscilations", False)
-drawOscilationGraph('extended_model_2_v3.txt', "model_2_oscilations", False)
-drawAmplitudeGraph("extended_model_1_amplitudes.txt", "model_1_amplitude", False)
-drawPeriodGraph("extended_model_1_periods.txt", "model_1_periods", False)
+drawOscilationGraph('extended_model_2_test.txt', "model_2_oscilations", False)
+#drawOscilationGraph('extended_model_2_v3.txt', "model_2_oscilations", False)
+drawAmplitudeGraph("extended_model_2_amplitudes_test.txt", "model_2_amplitude", False)
+drawPeriodGraph("extended_model_2_periods_test.txt", "model_2_periods", False)
